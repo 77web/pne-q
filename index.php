659e6be337352947cab0d38fc83c7b25c20ab36c
77web <info@77-web.com>
2013-04-05 12:08:00 +0900

    基本の流れ実装

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+use Silex\Provider\FormServiceProvider;
+use Silex\Provider\TranslationServiceProvider;
+use Silex\Provider\TwigServiceProvider;
+use My\QuestionType;
+
+require __DIR__.'/lib/vendor/autoload.php';
+
+$app = new \Silex\Application();
+$app['debug'] = true;
+$app->register(new FormServiceProvider());
+$app->register(new TwigServiceProvider(), array('twig.path' => __DIR__.'/lib/Resources/views'));
+$app->register(new TranslationServiceProvider(), array('locale' => 'ja'));
+
+$app['question_form'] = $app['form.factory']->create(new QuestionType());
+
+$app->get('/', function() use ($app){
+    return $app['twig']->render('form.html.twig', array('form' => $app['question_form']->createView()));
+});
+$app->post('/', function() use ($app){
+    $form = $app['question_form'];
+    $form->bind($app['request']);
+
+    if ($form->isValid())
+    {
+        return $app['twig']->render('show.html.twig', array('data' => $form->getData()));
+    }
+
+    return $app['twig']->render('form.html.twig', array('form' => $form->createView()));
+});
+
+$app->run();
